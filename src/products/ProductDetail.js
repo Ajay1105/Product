ad23f609d6719f65a6ce7d7cf34febc6bab4ffec
Ajay <aky11052003@gmail.com>
2023-06-05 10:18:46 +0530

    updated ProductDetail image address

--- src/products/ProductDetail.js
+++ src/products/ProductDetail.js
@@ -1,42 +1,42 @@
 var Product = [{
     name: "product 1",
     desc: "This is a description of an product. You had to give breif description of product here",
-    image: require("../assests/product.png")
+    image: require("./productImg/product.png")
 },
 {
     name: "Product 2",
     desc: "This is a description of an product. You had to give breif description of product here",
-    image: require("../assests/product.png")
+    image: require("./productImg/product.png")
 },{
     name: "Product 3",
     desc: "This is a description of an product. You had to give breif description of product here",
-    image: require("../assests/product.png")
+    image: require("./productImg/product.png")
 },{
     name: "product 5",
     desc: "This is a description of an product. You had to give breif description of product here",
-    image: require("../assests/product.png")
+    image: require("./productImg/product.png")
 },
 {
     name: "Product 5",
     desc: "This is a description of an product. You had to give breif description of product here",
-    image: require("../assests/product.png")
+    image: require("./productImg/product.png")
 },{
     name: "Product 6",
     desc: "This is a description of an product. You had to give breif description of product here",
-    image: require("../assests/product.png")
+    image: require("./productImg/product.png")
 },{
     name: "product 7",
     desc: "This is a description of an product. You had to give breif description of product here",
-    image: require("../assests/product.png")
+    image: require("./productImg/product.png")
 },
 {
     name: "Product 8",
     desc: "This is a description of an product. You had to give breif description of product here",
-    image: require("../assests/product.png")
+    image: require("./productImg/product.png")
 },{
     name: "Product 9",
     desc: "This is a description of an product. You had to give breif description of product here",
-    image: require("../assests/product.png")
+    image: require("./productImg/product.png")
 }]
 
 export default Product
